08bc29ce55ba295bfa2aaa74b24ae2c754a15eb7
mani1 <mani1@NANI143>
2022-08-10 21:51:27 +0530

    [Rajeswai] Added   Path Details

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,31 @@
+﻿Vusing Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    internal class FetchInventoryDetails
+    {
+        public Rice Read(string path)
+        {
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+
+        }
+    }
+}
--- Rice.cs
+++ Rice.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace InventoryManagement
 {
-    internal class Data
+    public class Rice
     {
+        public List<TypesofRice> typesofRice;
+        public List<TypesofPulses> typesofPulses;
+        public List<TypesofWheat> typesofWheat;
     }
+    public class TypesofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesofWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
 }

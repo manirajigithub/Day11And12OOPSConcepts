32cb982323c98a9af391185fc8caa7bc00e87c76
mani1 <mani1@NANI143>
2022-08-10 22:10:00 +0530

    [Rajeswari]Added Rice Details

---
+++ Rice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+
+{
+    public class Rice
+    {
+        public List<TypesofRice> typesofRice;
+        public List<TypesofPulses> typesofPulses;
+        public List<TypesofWheat> typesofWheat;
+    }
+    public class TypesofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+    public class TypesofWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+}

c020ffe78fb597c3121c04f9b6af311cecd14493
mani1 <mani1@NANI143>
2022-08-10 22:01:22 +0530

    [Rajeswari]Added  UC3

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace InventoryManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+
+            string filepath = "C:\\Users\\mani1\\source\\repos\\JSONInventoryManagement\\StockAccountManagement\\Data.json";
+
+            
+            
+            StockPortFolio port = new StockPortFolio();
+            port.DisplayStockDetails(filepath);
+        }
+
+
+    }
+}

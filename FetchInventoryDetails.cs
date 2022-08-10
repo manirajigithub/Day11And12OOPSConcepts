7260b02bcfefa099fedbf23ac936dbdd211de89d
mani1 <mani1@NANI143>
2022-08-10 22:08:16 +0530

    [Rajeswari]Added  Stock Details

--- FetchInventoryDetails.cs
+++ FetchInventoryDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace StockAccountManagement
 {
     internal class FetchInventoryDetails
     {
+        public Rice Read(string path)
+        {
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+
+        }
     }
 }
